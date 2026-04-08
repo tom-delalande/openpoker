6956680c16ac5ff70aa1bd7dddfc0b8a14774331
Tom Delalande <80664757+thomas-delalande@users.noreply.github.com>
2026-04-08 23:17:10 +0800

    everything is awesome i love fabrik

--- server/app/src/main/kotlin/domain/table/TableService.kt
+++ server/app/src/main/kotlin/domain/table/TableService.kt
@@ -3,7 +3,20 @@ package domain.table
 import domain.model.Table
 import java.time.Instant
 import java.util.UUID
+import kotlinx.serialization.ExperimentalSerializationApi
+import server.models.CommunityCardDealt
+import server.models.CommunityCardDealtType
+import server.models.HandEvent
+import server.models.HandEventCommunityCardDealt
+import server.models.HandEventHandStarted
+import server.models.HandEventHandStartedEvent
+import server.models.HandEventRoundStarted
+import server.models.HandStarted
 import server.models.HandStartedEvent
+import server.models.HandStartedEventType
+import server.models.HandStartedType
+import server.models.RoundStarted
+import server.models.RoundStartedType
 
 class TableService(
     val activeRepository: ActiveTableStateRepository,
@@ -34,15 +47,39 @@ class TableService(
         table.processPlayerAction(playerId, action, now)
     }
 
-    fun Table.toEvents(): List<HandStartedEvent> = buildList {
+    @OptIn(ExperimentalSerializationApi::class)
+    fun Table.toEvents(): List<HandEvent> = buildList {
         rounds.forEach { round ->
             if (round.id == 0) {
-                add(HandStartedEvent())
+                add(
+                    HandEventHandStarted(
+                        value = HandStarted(
+                            type = HandStartedType.HAND_STARTED_EVENT,
+                            dealerButton = dealerSeat,
+                        )
+                    )
+                )
             }
-            // add(RoundStartedEvent)
+            add(
+                HandEventRoundStarted(
+                    value = RoundStarted(
+                        type = RoundStartedType.ROUND_STARTED_EVENT,
+                        street = round.street.name
+                    )
+                )
+            )
+
             round.actions.map { action ->
                 when (action) {
-                    is Table.Round.Action.DealCommunityCards -> add(TableEvent())
+                    is Table.Round.Action.DealCommunityCards -> add(
+                        HandEventCommunityCardDealt(
+                            value = CommunityCardDealt(
+                                type = CommunityCardDealtType.COMMUNITY_CARD_DEALT_EVENT,
+                                cards = action.cards.map { it.toStringValue() }
+                            )
+                        )
+                    )
+
                     is Table.Round.Action.PlayerAction.AddChips -> TODO()
                     is Table.Round.Action.PlayerAction.Bet -> TODO()
                     is Table.Round.Action.PlayerAction.Call -> TODO()
@@ -64,5 +101,15 @@ class TableService(
                 }
             }
         }
+    }
+
+    fun Table.Card.toStringValue(): String {
+        val suit = when (suit) {
+            Table.Card.Suit.Hearts -> "h"
+            Table.Card.Suit.Diamonds -> "d"
+            Table.Card.Suit.Spades -> "s"
+            Table.Card.Suit.Clubs -> "c"
+        }
+        return "$rank$suit"
     }
 }
